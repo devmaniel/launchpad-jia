d1b7d2acc686763aab8450d0de382653d11c9f85
Melco Maniel M. Gulbe <devmaniel@gmail.com>
2025-11-06 17:57:49 +0800

    Style: Adjusting Layout.tsx styling based on the figma design file

--- src/app/recruiter-dashboard/layout.tsx
+++ src/app/recruiter-dashboard/layout.tsx
@@ -110,7 +110,16 @@ export default function Layout({ children }) {
         <div
           className="main-content bg-white"
           id="panel"
-          style={{ marginTop: "32px", height: "100vh", border: "1px solid #EAECF5", borderTopLeftRadius: "24px", borderTopRightRadius: "24px", overflowY: "scroll" }}
+          style={{
+            marginTop: "22px",
+            marginLeft: "266px",
+            marginRight: "32px",
+            marginBottom: "22px",
+            width: "calc(100vw - 290px)",
+            minHeight: "calc(100vh - 22px)",
+            border: "1px solid #EAECF5",
+            borderRadius: "24px",
+          }}
         >
           {children}
         </div>
